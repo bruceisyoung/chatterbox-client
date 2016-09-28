16d18d52ad2e194ec99cd2d3b64e66cc5bb7aa3f
bruceisyoung <bruce.isyoung2016@gmail.com>
2016-09-27 18:53:17 -0700

    finish some prompts in advance requirement
    -[x] Construct a Parse query that responds with only messages from a specific room.
    -[x] Allow users to have more than one room open at a time using tabs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,110 +1,54 @@
 // YOUR CODE HERE:
-$(document).ready(() => {
-  app.init();
-  //changed the way the call app.fetch;
-  app.fetch({roomname: 'lobby'});
-
-  $('#submitRoom').on('click', () => {
-    var newRoom = $('#newRoom').val();
-  //we need to find out whether the room has existed or not
-    if (!checkRoom(newRoom)) {
-      app.renderRoom(newRoom);
-    }
-  });
-
-  $('#refresh').on('click', () => {
-    app.clearMessages();
-    app.fetch();
-  });
-
-  $('#gotoNewRoom').on('click', () => {
-    var newRoom = $('#newRoom').val();
-    if (!checkRoom(newRoom)) {
-      app.renderRoom(newRoom);
-    }
-    if (!checkRoomTab(newRoom)) {
-      createRoomTab(newRoom);
-    }
-    console.log($('.tab').children());
-    console.log($('.tab').children().length);
-  });
-
-  $('#send').on('submit', () => {
-    event.preventDefault();
-    var username = document.URL.slice(_.lastIndexOf(document.URL, '=') + 1);
-    var messageContent = $('#message').val();
-    var roomSelect = document.getElementById('roomSelect');
-    var roomSelected = roomSelect.options[roomSelect.selectedIndex].text;
-    var message = {
-      username: username,
-      text: messageContent,
-      roomname: roomSelected
-    };
-    app.send(message);
-  });
-
-  $(document).on('click', '.tweetUser', function() {
-    if (_.indexOf(app.friends, $(this).text()) === -1) {
-      app.friends.push($(this).text());
-    }
-    app.clearMessages();
-    app.fetch();
-  });
-});
-
 const app = {};
 
 app.init = () => {
-  app.server = 'https://api.parse.com/1/classes/messages';
-  app.friends = [];
-  app.renderRoom('lobby');
+  app.server = 'https://api.parse.com/1/classes/messages'; //store the server address
+  app.friends = [];                   //create a global array to store friends
+  app.renderRoom('lobby');            //make lobby the default room
+  $('.tab #lobby').addClass('active'); //make active as the default state of lobby in the tab area 
+  app.fetch({roomname: 'lobby'});
 };
 
-app.send = (message) => {
+app.clearMessages = () => $('#chats').html('');
+
+app.send = (message) => {                                           //notice the format of ajax data request
   $.ajax({
-    // This is the url you should use to communicate with the parse API server.
     url: app.server,
     type: 'POST',
     data: JSON.stringify(message),
     contentType: 'application/json',
     success: (data) => {
-      console.log('chatterbox: Message sent');
+      app.clearMessages();
+      app.fetch({roomname: $("#roomSelect").val()});
     },
     error: (data) => {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message', data);
     }
   });
 };
 
-app.fetch = (filterObject) => {
+app.fetch = (filterObject) => {                                     //notice the format of ajax data request
   $.ajax({
-  // This is the url you should use to communicate with the parse API server.
     url: app.server,
     type: 'GET',
     contentType: 'application/json',
-    //data: {where: {roomname: 'lobby', username: 'sjfkdlsa'}, limit: 50, order: '-createdAt'},
-    data: {where: filterObject, limit: 50, order: '-createdAt'},
+    data: {where: filterObject, limit: 50, order: '-createdAt'},    //data parse format: {where: {roomname: 'lobby', username: 'sjfkdlsa'}, limit: 50, order: '-createdAt'},
     success: (data) => {
-      // 'order=-updatedAt';
       _.each(data.results, (datum) => {
         app.renderMessage(datum);
       });
     },
     error: (data) => {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to send message', data);
     }
   });
 
 };
 
-app.clearMessages = () => $('#chats').empty();
-
 app.renderMessage = (message) => {
   var tweet;
   var user = $('<a class="tweetUser" href="#"></a>').text(message.username);
-  var time = $('<span class="tweetTime"></span>').text(`${jQuery.timeago(message.updatedAt)}`);
+  var time = $('<span class="tweetTime"></span>').text(`${jQuery.timeago(message.updatedAt)}`);   //use timeage module to display the time in a user-friendly way
   var msg = $('<p class="tweetMessage"></p>').text(message.text);
   $tweets = $('<div class="tweet"></div>').append(user).append(time).append(msg);
   if ((_.indexOf(app.friends, message.username) !== -1)) {
@@ -114,11 +58,9 @@ app.renderMessage = (message) => {
 };
 
 app.renderRoom = (roomName) => {
-  var childLength = $('#roomSelect').children().length;
-  var newRoom = `<option value = ${childLength + 1}>${roomName}</option>"`;
+  var newRoom = `<option value = "${roomName}">${roomName}</option>"`;
   $('#roomSelect').append($(newRoom));
 };
-
 
 var checkRoom = (roomname) => {
   var childLength = $('#roomSelect').children().length;
@@ -138,9 +80,74 @@ var checkRoomTab = (roomname) => {
     }
   }
   return false;
-}
+};
 
 var createRoomTab = (roomname) => {
-  newRoomTab = `<li><a href="#" class="tablinks" onclick="openCity(event, 'London')">${roomname}</a></li>`;
+  newRoomTab = `<li><a href="#" id="${roomname}" onclick="setActive(event, '${roomname}')">${roomname}</a></li>`;
   $('.tab').append($(newRoomTab));
-}
+};
+
+//need to refactor this function
+//when a tab is clicked, 
+var setActive = (event, roomname) => {
+  var childLength = $('.tab').children().length;
+  for(var i = 0; i < childLength; i++) {
+    $($($('.tab').children()[i]).children()[0]).removeClass('active');
+    if(roomname === $($('.tab').children()[i]).text()){
+      $($($('.tab').children()[i]).children()[0]).addClass('active');
+    }
+  }
+  setSelectOption(roomname);
+  app.clearMessages();
+  app.fetch({roomname: roomname});
+};
+
+var setSelectOption = (roomname) => {
+  $("#roomSelect").val(roomname);
+};
+
+$(document).ready(() => {
+  //changed the way the call app.fetch;
+  app.init();
+
+  $('#refresh').on('click', () => {
+    app.clearMessages();
+    app.fetch({roomname: $("#roomSelect").val()});
+  });
+
+  $('#gotoRoom').on('click', () => {
+    var newRoom = $('#newRoom').val();
+    if (!checkRoom(newRoom)) {
+      app.renderRoom(newRoom);
+    }
+    if (!checkRoomTab(newRoom)) {
+      createRoomTab(newRoom);
+    } 
+    setSelectOption(newRoom);
+    setActive(null, newRoom);
+  });
+
+  $('#send').on('submit', () => {
+    event.preventDefault();
+    var username = document.URL.slice(_.lastIndexOf(document.URL, '=') + 1);
+    var messageContent = $('#message').val();
+    var roomSelect = document.getElementById('roomSelect');
+    var roomSelected = roomSelect.options[roomSelect.selectedIndex].text;
+    var message = {
+      username: username,
+      text: messageContent,
+      roomname: roomSelected
+    };
+    app.send(message);
+
+  });
+
+  $(document).on('click', '.tweetUser', function() {
+    if (_.indexOf(app.friends, $(this).text()) === -1) {
+      app.friends.push($(this).text());
+    }
+    app.clearMessages();
+    app.fetch({roomname: $("#roomSelect").val()});
+  });
+});
+
